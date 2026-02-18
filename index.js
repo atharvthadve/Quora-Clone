9075c5f1c538e01f7b1e64fec71f7ef7f3bec309
Atharv08 <atharvthadve08@gmail.com>
2026-02-19 03:32:00 +0530

    version-1

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 2100;
 const path = require("path");
 const {v4: uuidv4} = require("uuid")
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
 
 app.use(express.urlencoded({extended: true}))
 
@@ -34,9 +36,9 @@ app.get("/posts", (req, res)=>{
     res.render("index.ejs", {posts});
 })
 
-app.get("/posts/new", ((req, res)=>{
+app.get("/posts/new", (req, res)=>{
     res.render("newpost.ejs");
-}))
+})
 
 app.post("/posts", (req, res)=>{
     let id = uuidv4();
@@ -49,13 +51,32 @@ app.post("/posts", (req, res)=>{
 app.get("/posts/:id", (req, res) => {
     let { id } = req.params;
     let post = posts.find((p) => p.id === id);
-    console.log(post);
     if (post) {
         res.render("detail.ejs", { post });  
     } else {
         res.render("error.ejs");
     }
 });
+
+app.patch("/posts/:id", (req, res)=>{
+    let {id} = req.params;
+    let content = req.body.content;
+    let post = posts.find((p)=> id === p.id);
+    post.content = content;
+    res.redirect("/posts");
+})
+
+app.get("/posts/:id/edit", (req, res)=>{
+    let {id} = req.params;
+    let post = posts.find((p)=> id === p.id)
+    res.render("edit.ejs", {post});
+})
+
+app.delete("/posts/:id", (req,res)=>{
+    let {id} = req.params;
+    posts = posts.filter((p)=> id !== p.id)
+    res.redirect("/posts")
+})
 
 app.listen(port, ()=>{
     console.log(`Server started at http://localhost:${port}`);
